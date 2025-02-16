27abf0f7be11b48c6ee2e8eab22f1958a2268be0
CaioLima10 <caiohenrys50@gmail.com>
2025-02-16 14:12:06 -0300

    middleware tratamento de error do projeto

---
+++ src/middlewares/error-handling.ts
@@ -0,0 +1,23 @@
+import { Response, Request, NextFunction } from "express";
+import { AppError } from "@/utils/app-error";
+import { ZodError } from "zod"
+
+export function errorHandling(
+  error: any,
+  response: Response,
+  request: Request,
+  next: NextFunction
+) {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({ message: error.message })
+  }
+
+  if (error instanceof ZodError) {
+    return response.status(400).json({
+      message: "validation error",
+      issues: error.format()
+    })
+  }
+
+  return response.status(500).json({ message: error.message })
+}
